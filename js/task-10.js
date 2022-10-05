0450bb30015a9a31a229e752b0abb2c8191cc133
MightyLex <MightyLex@gmail.com>
2022-10-06 00:07:50 +0300

    finally

--- js/task-10.js
+++ js/task-10.js
@@ -2,8 +2,46 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-const crtBtn = document.querySelector('button[data-create]')
-const dstrBtn = document.querySelector('button[data-destroy]')
-const amount = document.querySelector('input')
-const boxArea = document.querySelector('#boxes')
+const crtBtnRef = document.querySelector('button[data-create]')
+const dstrBtnRef = document.querySelector('button[data-destroy]')
+const inputRef = document.querySelector('input')
+const boxAreaRef = document.querySelector('#boxes')
 
+
+// -----------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------
+
+inputRef.addEventListener('change', (event) => {
+  // zdes nado mb nado prosto target ?
+  const quantity = event.currentTarget.value
+  crtBtnRef.addEventListener("click", createBoxes(quantity))
+  
+})
+
+
+function createBoxes(amount) {
+  // console.log(amount);
+  for (let i = 0; i < amount; i += 1) {
+    // boxAreaRef.insertAdjacentHTML('afterbegin', '<div class="addedDiv">')
+    const newDiv = document.createElement('div')
+    const color = getRandomHexColor()
+    newDiv.classList.add('addedDiv')
+
+    const size = 30+10*i
+    // console.log(size);
+    newDiv.style.width = size +"px";
+    newDiv.style.height = size +"px";
+    newDiv.style.backgroundColor = color;  
+    boxAreaRef.prepend(newDiv)
+    
+  }
+}
+
+
+
+
+dstrBtnRef.addEventListener('click', destroyBoxes)
+
+function destroyBoxes() {
+  boxAreaRef.innerHTML = ''
+}
